8a9fda3aab06ebdd7a57ce161834c7b4cc748e3e
BiLiang <445143561@qq.com>
2023-07-12 14:00:18 +0800

    React文章管理系统模板-MenuHeader头部组件优化

--- src/components/MenuHeader/index.jsx
+++ src/components/MenuHeader/index.jsx
@@ -1,26 +1,76 @@
 import {
+  DownOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+} from "@ant-design/icons";
+import { Layout, Dropdown, Menu, Space, Avatar } from "antd";
+import React, { useState } from "react";
 
-} from '@ant-design/icons';
-import { Layout,  } from 'antd';
-import React, { useState } from 'react';
-const { Header} = Layout;
+const { Header } = Layout;
 
 export default function MenuHeader() {
   const [collapsed, setCollapsed] = useState(false);
-
+  const changeFoldOutlined = () => {
+    setCollapsed(!collapsed);
+  };
+  const menu = (
+    <Menu
+      items={[
+        // {
+        //   key: "1",
+        //   label: (
+        //     <a
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //       href="https://www.antgroup.com"
+        //     >
+        //       退出登录
+        //     </a>
+        //   ),
+        // },
+        {
+          key: "2",
+          danger: true,
+          label: "退出登录",
+        },
+      ]}
+    />
+  );
   return (
     <Header
-    className="site-layout-background"
-    style={{
-      padding: 0,
-    }}
-  >
-    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-      className: 'trigger',
-      onClick: () => setCollapsed(!collapsed),
-    })}
-  </Header>
-  )
+      className="site-layout-background"
+      style={{
+        padding: "0 16px",
+      }}
+    >
+      {collapsed ? (
+        <MenuUnfoldOutlined onClick={changeFoldOutlined} />
+      ) : (
+        <MenuFoldOutlined onClick={changeFoldOutlined} />
+      )}
+      <div
+        style={{
+          float: "right",
+        }}
+      >
+        <Dropdown overlay={menu}>
+          {/* <a onClick={(e) => e.preventDefault()}> */}
+          <Space>
+            <span>
+              <Avatar
+                size={40}
+                style={{
+                  backgroundColor: "#f56a00",
+                }}
+              >
+                admin
+              </Avatar>
+            </span>
+            <DownOutlined />
+          </Space>
+          {/* </a> */}
+        </Dropdown>
+      </div>
+    </Header>
+  );
 }
